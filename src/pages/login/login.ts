47748d1e64a67e9ddd1b0092abdb73e047628183
suvojitkar <karsuvojit4@gmail.com>
2017-08-05 22:52:07 +0530

    login error msg added in console

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -67,7 +67,7 @@ export class LoginPage {
       }, function(error){
         setTimeout(() => {
                 loading.dismiss();
-                nav.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
+                //nav.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
                 console.log(error);
                 }, 1000);
       });
@@ -107,7 +107,7 @@ export class LoginPage {
       }, function (error) {
         setTimeout(() => {
             loading.dismiss();
-            nav.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
+            //nav.setRoot(HomePage, {}, {animate: true, direction: 'forward'});
             console.log(error);
          }, 1000);
 
